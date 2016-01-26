48ca7cc42c7a61362a1422366f983c771638543f
r-tanner-f <robert.tanner.faulkner@gmail.com>
2016-01-26 14:16:38 -0800

    Add barebones example of use

---
+++ examples/simple.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var Express = require('express');
+var app = Express();
+var router = Express.Router();
+
+var session = require('express-session');
+
+var permissions = require('../src/index.js');
+var MemoryPermits = require('../src/index.js').MemoryPermits;
+var permit = require('../src/index.js').permit;
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+}));
+app.use(permissions({
+  //store: new MongoPermits(options)
+  store: new MemoryPermits({
+    'awesome-user' : {
+      'have-fun' : true,
+    },
+    'terrible-user' : {
+      'have-fun' : false,
+    },
+  }),
+  username: 'req.session.username'
+}));
+
+router.get('/', permit('have-fun'), function(req, res) {
+  res.send('yaaay');
+});
+
+router.get('/login/awesome-user', function(req, res) {
+  req.session.username = 'awesome-user';
+  res.send('Logged in as awesome-user');
+});
+
+router.get('/login/terrible-user', function(req, res) {
+  req.session.username = 'terrible-user';
+  res.send('Logged in as terrible-user');
+});
+
+app.use(router);
+
+// Error handler
+app.use(function(err, req, res, next) { //jshint ignore:line
+  res.status(403).send('Go away!!');
+});
+
+app.listen(3000, function() {
+  console.log('For a good time, go to port 3000');
+});
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 module.exports = function Permit (app) {
   app.permit = function(req, res, next) {
     console.log('Configuring permissions');
@@ -12,3 +12,42 @@ module.exports = function Permit (app) {
     }
   };
 };
+*/
+/*
+ * Three ways to collect permissions
+ * 1) Manually
+ * 2) On the module
+ * 3) On the router
+ */
+
+function expressPermit (options) {
+  var store = options.store;
+  return function(req, res, next) {
+    req.permits = store.get(eval(options.username));
+    next();
+  };
+}
+
+expressPermit.permit = function permit(name) {
+  return function(req, res, next) {
+    if (!req.permits[name]) {
+      next('Permissions Error');
+    }
+    next();
+  };
+};
+
+function MemoryPermits(permissions) {
+  this.permissions = permissions;
+
+  this.get = function get(id) {
+    return this.permissions[id];
+  };
+
+  this.set = function set(id, permissions) {
+    this.permissions[id] = permissions;
+  };
+}
+expressPermit.MemoryPermits =  MemoryPermits;
+
+module.exports = expressPermit;
